3b1ce6e9a8115e60b0c10fcc61013ab8596ad418
Albert Mensah <albertoforimensah04@gmail.com>
2024-06-24 09:13:16 +0000

    vercel analytics added

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import { DataProvider } from "./reducers";
+import { Analytics } from '@vercel/analytics/react';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -15,6 +16,7 @@ root.render(
   <BrowserRouter>
     <DataProvider>
       <App />
+      <Analytics />
     </DataProvider>
   </BrowserRouter>
 );
